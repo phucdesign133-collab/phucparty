f39948276f2aed244c6672a4105665ce6a710bda
phucdesign <phucdesign133@gmail.com>
2026-09-18 10:24:13 +0700

    fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/phucparty/",
   publicDir: "public", // Ép buộc lấy folder này để copy vào root của dist
   build: {
     outDir: "dist",
